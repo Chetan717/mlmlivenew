9e277822a70f5e61a21e95a0c1227432e6400b98
chetan717 <chaitanyachaudhari717@gmail.com>
2026-05-29 10:03:29 +0530

    second

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,14 @@ import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { GeneralContext } from "./Context/GeneralContext.jsx";
 import { Toast } from "@heroui/react";
-import ScrollToTop from "./Pages/ScrollToTop.js";
+// import ScrollToTop from "./Pages/ScrollToTop.js";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GeneralContext>
       <Toast.Provider />
       <BrowserRouter>
-        <ScrollToTop />
+        {/* <ScrollToTop /> */}
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
